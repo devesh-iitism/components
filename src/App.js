8b25855eb0d2f96b8e75717eb7522729acf97e09
Devesh Chaturvedi <dcdev001@gmail.com>
2023-08-11 16:46:06 +0530

    implement controlled component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import { useState } from "react";
 import Dropdown from "./components/Dropdown";
 
 function App() {
+    const [selection, setSelection] = useState();
+
+    const handleSelect = (option) => {
+        setSelection(option);
+    };
+
     const options = [
         { label: 'Red', value: 'red' },
         { label: 'Green', value: 'green' },
         { label: 'Blue', value: 'blue' }
     ];
-    return <Dropdown options={options}/>
+    return <Dropdown selection={selection} onSelect={handleSelect} options={options}/>
 }
 
 export default App;
